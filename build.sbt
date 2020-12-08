93e0786d5c750bf5956fe5fa1a9d73a5c0b00ff6
Sam Sloman <samsloman@live.co.uk>
2020-12-08 15:52:35 +0000

    BDOG-1178: Updates to Play 27

--- build.sbt
+++ build.sbt
@@ -1,11 +1,10 @@
 import uk.gov.hmrc.DefaultBuildSettings.integrationTestSettings
-import uk.gov.hmrc.SbtArtifactory
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin.publishingSettings
 
 val appName = "health-indicators"
 
 lazy val microservice = Project(appName, file("."))
-  .enablePlugins(play.sbt.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin, SbtArtifactory)
+  .enablePlugins(play.sbt.PlayScala, SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin)
   .settings(
     majorVersion                     := 0,
     libraryDependencies              ++= AppDependencies.compile ++ AppDependencies.test
@@ -15,4 +14,4 @@ lazy val microservice = Project(appName, file("."))
   .settings(integrationTestSettings(): _*)
   .settings(resolvers += Resolver.jcenterRepo)
   .settings(PlayKeys.playDefaultPort := 9018)
-  .settings(scalaVersion := "2.12.10")
+  .settings(scalaVersion := "2.12.11")
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,18 +4,17 @@ object AppDependencies {
   import play.core.PlayVersion
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-play-26"  % "1.3.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-26" % "0.21.0",
-    "org.typelevel"     %% "cats-core"          % "1.6.1"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-27"  % "3.0.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-27"         % "0.21.0",
+    "org.typelevel"     %% "cats-core"                  % "1.6.1"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-play-26"  % "1.3.0"             % Test classifier "tests",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test"    % "0.21.0"            % Test,
-    "org.scalatest"          %% "scalatest"          % "3.1.0"             % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3"             % Test,
-    "com.typesafe.play"      %% "play-test"          % PlayVersion.current % Test,
-    "org.mockito"            %% "mockito-scala"      % "1.10.2"            % Test
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test"             % "0.21.0"            % Test,
+    "org.scalatest"          %% "scalatest"                   % "3.1.2"             % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"          % "4.0.0"             % Test,
+    "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % Test,
+    "org.mockito"            %% "mockito-scala"               % "1.10.2"            % Test
   )
 
 }
